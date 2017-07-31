5a9fd37d0294aff559cc2bf5d4c29be3ea6de685
meij03 <meij03@users.noreply.github.com>
2017-07-31 14:25:26 -0400

    Create client.js

---
+++ public/client.js
@@ -0,0 +1,10 @@
+        .parallax { 
+ background-image: url("https://media.giphy.com/media/qajXB8w1cnVOo/giphy.gif");
+
+    min-height: 1000px; 
+
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
